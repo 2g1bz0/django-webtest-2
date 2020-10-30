94a53ca061f439290135e88b939af40d61360098
Your Name <you@example.com>
2020-10-30 12:22:27 +0000

    init

--- website/views.py
+++ website/views.py
@@ -4,5 +4,20 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name="index.html"
     
+    def get_context_data(self):
+        ctxt=super().get_context_data()
+        ctxt["username"]="hay"
+        return ctxt
+    
 class AboutView(TemplateView):
     template_name="about.html"
+    
+    def get_context_data(self):
+        ctxt=super().get_context_data()
+        ctxt["num_services"]=119212321312
+        ctxt["skills"]=[
+            "python",
+            "js",
+            "c",
+            ]
+        return ctxt
